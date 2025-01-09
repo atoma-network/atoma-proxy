daa04347b4a66a99a7bc1d9a82bd615f5d5619c8
Francisco Melo <43780565+francis2tm@users.noreply.github.com>
2025-01-09 15:29:14 +0000

    chore: create response for nodes_create (#133)
    * chore: create response for nodes_create
    
    * refactor: remove unused import in nodes handler
    
    - Removed the unused `serde_json::Value` import from the nodes handler file to clean up the code and improve readability.

--- atoma-proxy/src/server/handlers/nodes.rs
+++ atoma-proxy/src/server/handlers/nodes.rs
@@ -8,7 +8,6 @@ use blake2::digest::consts::U32;
 use blake2::digest::generic_array::GenericArray;
 use blake2::{Blake2b, Digest};
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 use sui_sdk::types::base_types::SuiAddress;
 use sui_sdk::types::crypto::{PublicKey as SuiPublicKey, Signature, SuiSignature};
 use tokio::sync::oneshot;
@@ -64,6 +63,12 @@ pub struct NodesCreateRequest {
     signature: String,
 }
 
+#[derive(Clone, Debug, Deserialize, Serialize, ToSchema)]
+pub struct NodesCreateResponse {
+    /// The message of the response
+    message: String,
+}
+
 /// Create node
 ///
 /// This endpoint allows nodes to register or update their public address in the system.
@@ -88,7 +93,7 @@ pub struct NodesCreateRequest {
     post,
     path = "",
     responses(
-        (status = OK, description = "Node public address registered successfully", body = Value),
+        (status = OK, description = "Node public address registered successfully", body = NodesCreateResponse),
         (status = INTERNAL_SERVER_ERROR, description = "Failed to register node public address")
     )
 )]
@@ -96,7 +101,7 @@ pub struct NodesCreateRequest {
 pub async fn nodes_create(
     State(state): State<ProxyState>,
     Json(payload): Json<NodesCreateRequest>,
-) -> Result<Json<Value>, AtomaProxyError> {
+) -> Result<Json<NodesCreateResponse>, AtomaProxyError> {
     let base64_signature = &payload.signature;
     let body_bytes =
         serde_json::to_vec(&payload.data).map_err(|e| AtomaProxyError::InvalidBody {
@@ -185,7 +190,9 @@ pub async fn nodes_create(
             endpoint: NODES_CREATE_PATH.to_string(),
         })?;
 
-    Ok(Json(Value::Null))
+    Ok(Json(NodesCreateResponse {
+        message: "Success".to_string(),
+    }))
 }
 
 /// The response body for selecting a node's public key for encryption
