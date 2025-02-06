4a28f0afd374c3263fead052555729be641225c2
Jorge António <matroid@outlook.com>
2025-02-06 11:46:49 +0000

    feat: add proper authentication for models path (#204)
    * first commit
    
    * fmt

--- atoma-proxy/src/server/middleware.rs
+++ atoma-proxy/src/server/middleware.rs
@@ -14,9 +14,10 @@ use serde_json::Value;
 use tracing::instrument;
 
 use super::{
+    check_auth,
     error::AtomaProxyError,
     handlers::{
-        image_generations::CONFIDENTIAL_IMAGE_GENERATIONS_PATH,
+        image_generations::CONFIDENTIAL_IMAGE_GENERATIONS_PATH, models::MODELS_PATH,
         nodes::MAX_NUM_TOKENS_FOR_CONFIDENTIAL_COMPUTE, update_state_manager,
     },
     http_server::ProxyState,
@@ -230,6 +231,11 @@ pub async fn authenticate_middleware(
 ) -> Result<Response> {
     let (mut req_parts, body) = req.into_parts();
     let endpoint = req_parts.uri.path().to_string();
+    if endpoint == MODELS_PATH {
+        check_auth(&state.state_manager_sender, &req_parts.headers, &endpoint).await?;
+        let req = Request::from_parts(req_parts, body);
+        return Ok(next.run(req).await);
+    }
     let body_bytes = axum::body::to_bytes(body, MAX_BODY_SIZE)
         .await
         .map_err(|e| {
