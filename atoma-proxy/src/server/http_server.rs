b6450ecf26e50ba01b556fab6ee524e6cba452e3
Jorge Antonio <matroid@outlook.com>
2025-03-31 14:51:57 +0100

    change order of middleware

--- atoma-proxy/src/server/http_server.rs
+++ atoma-proxy/src/server/http_server.rs
@@ -203,11 +203,11 @@ pub fn create_router(state: &ProxyState) -> Router {
                 ServiceBuilder::new()
                     .layer(from_fn_with_state(
                         state.clone(),
-                        handle_locked_stack_middleware,
+                        confidential_compute_middleware,
                     ))
                     .layer(from_fn_with_state(
                         state.clone(),
-                        confidential_compute_middleware,
+                        handle_locked_stack_middleware,
                     )),
             ),
         )
@@ -216,9 +216,9 @@ pub fn create_router(state: &ProxyState) -> Router {
                 ServiceBuilder::new()
                     .layer(from_fn_with_state(
                         state.clone(),
-                        handle_locked_stack_middleware,
+                        authenticate_middleware,
                     ))
-                    .layer(from_fn_with_state(state.clone(), authenticate_middleware)),
+                    .layer(from_fn_with_state(state.clone(), handle_locked_stack_middleware)),
             ),
         )
         .merge(node_routes)
