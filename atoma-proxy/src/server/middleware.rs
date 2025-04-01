451c8d1ced35cbcb3e07d0064116a9cfd091489a
Martin Stefcek <gcifko@gmail.com>
2025-04-01 17:05:56 +0400

    fix

--- atoma-proxy/src/server/middleware.rs
+++ atoma-proxy/src/server/middleware.rs
@@ -1437,6 +1437,7 @@ pub mod auth {
     /// * `user_id` - The ID of the user requesting the stack
     /// * `task_small_id` - The small ID of the task that the user is requesting
     /// * `endpoint` - The API endpoint being accessed
+    /// * `total_tokens` - The estimated total number of tokens for the request
     ///
     /// # Returns
     /// * `Result<SelectedNodeMetadata>` - Stack metadata if successful
