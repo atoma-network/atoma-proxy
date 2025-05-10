2b594cd0d4f274662a82f3d729294be83f213392
Jorge António <matroid@outlook.com>
2025-05-10 10:35:48 +0100

    fix: fix issues for completions endpoint (#464)
    * fix
    
    * handle completions endpoint
    
    * handle completions endpoint
    
    * fmt
    
    * changes
    
    * further fixes
    
    * further fixes
    
    * resolve conflicts

--- atoma-proxy/src/server/streamer.rs
+++ atoma-proxy/src/server/streamer.rs
@@ -482,9 +482,36 @@ impl Stream for Streamer {
                     };
 
                     if let Some(usage) = chunk.get(USAGE) {
-                        self.status = StreamStatus::Completed;
+                        if !usage.is_null() {
+                            self.status = StreamStatus::Completed;
+                            self.handle_final_chunk(usage, chunk.get(RESPONSE_HASH_KEY))?;
+                            if !choices.is_empty() {
+                                trace!(
+                                target = "atoma-service-streamer",
+                                level = "trace",
+                                "Client disconnected before the final chunk was processed, using usage transmitted by the node last live chunk to update the stack num tokens"
+                            );
+                            }
+                        }
+                    }
+                } else if self.endpoint == COMPLETIONS_PATH {
+                    let Some(choices) = chunk.get(CHOICES).and_then(|choices| choices.as_array())
+                    else {
+                        error!(
+                            target = "atoma-service-streamer",
+                            level = "error",
+                            "Error getting choices from chunk"
+                        );
+                        return Poll::Ready(Some(Err(Error::new(
+                            "Error getting choices from chunk",
+                        ))));
+                    };
 
-                        self.handle_final_chunk(usage, chunk.get(RESPONSE_HASH_KEY))?;
+                    if let Some(usage) = chunk.get(USAGE) {
+                        if !usage.is_null() {
+                            self.status = StreamStatus::Completed;
+                            self.handle_final_chunk(usage, chunk.get(RESPONSE_HASH_KEY))?;
+                        }
                         if !choices.is_empty() {
                             trace!(
                                 target = "atoma-service-streamer",
@@ -494,8 +521,10 @@ impl Stream for Streamer {
                         }
                     }
                 } else if let Some(usage) = chunk.get(USAGE) {
-                    self.status = StreamStatus::Completed;
-                    self.handle_final_chunk(usage, chunk.get(RESPONSE_HASH_KEY))?;
+                    if !usage.is_null() {
+                        self.status = StreamStatus::Completed;
+                        self.handle_final_chunk(usage, chunk.get(RESPONSE_HASH_KEY))?;
+                    }
                 }
                 self.num_generated_tokens += 1;
                 Poll::Ready(Some(Ok(Event::default().json_data(&chunk)?)))
