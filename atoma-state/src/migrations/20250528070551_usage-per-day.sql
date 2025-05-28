24e1c469a474c5eadbb4c0e0a425cc86a61cd6f5
Martin Stefcek <gcifko@gmail.com>
2025-05-28 12:41:59 +0200

    rename timestamp to date

--- atoma-state/src/migrations/20250528070551_usage-per-day.sql
+++ atoma-state/src/migrations/20250528070551_usage-per-day.sql
@@ -1,11 +1,11 @@
 CREATE TABLE
   IF NOT EXISTS usage_per_day (
     user_id BIGINT NOT NULL,
-    timestamp DATE DEFAULT CURRENT_DATE NOT NULL,
+    date DATE DEFAULT CURRENT_DATE NOT NULL,
     model TEXT NOT NULL,
     input_amount BIGINT NOT NULL DEFAULT 0,
     input_tokens BIGINT NOT NULL DEFAULT 0,
     output_amount BIGINT NOT NULL DEFAULT 0,
     output_tokens BIGINT NOT NULL DEFAULT 0,
-    UNIQUE (user_id, timestamp, model)
+    UNIQUE (user_id, date, model)
   );
